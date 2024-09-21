5128e9aca83edde8631c512f22a3d73b63b09afd
Vihanga Amarasinghe Z <142644936+vihanga886@users.noreply.github.com>
2024-09-21 19:39:25 +0530

    Add files via upload

---
+++ src/mysoftware123/Mysoftware123.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mysoftware123;
+
+/**
+ *
+ * @author USER
+ */
+public class Mysoftware123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new NewJFrame().setVisible(true);
+    }
+}
